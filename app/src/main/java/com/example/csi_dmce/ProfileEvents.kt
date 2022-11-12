e059dfedaffb343592670f1ffdf7d12efb7526e2
Zaurez <zaureztimmapuri@gmail.com>
2022-11-12 23:22:40 +0530

    profile added

---
+++ app/src/main/java/com/example/csi_dmce/ProfileEvents.kt
@@ -0,0 +1,4 @@
+package com.example.csi_dmce
+
+data class ProfileEvents(var title_heading : String, var Description : String)
+
---
+++ app/src/main/java/com/example/csi_dmce/ProfileEventsAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.csi_dmce
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.contracts.Returns
+
+class ProfileEventsAdapter (private val eventList : ArrayList<ProfileEvents>) : RecyclerView.Adapter<ProfileEventsAdapter.MyViewHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder
+    {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.profile_events,parent,false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int)
+    {
+       val currentItem = eventList[position]
+        holder.titleHeading.text = currentItem.title_heading
+        holder.description.text=currentItem.Description
+    }
+
+    override fun getItemCount(): Int
+    {
+        return eventList.size
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    {
+        val titleHeading : TextView = itemView.findViewById(R.id.Event)
+        val description : TextView = itemView.findViewById(R.id.Description)
+    }
+
+
+
+}
