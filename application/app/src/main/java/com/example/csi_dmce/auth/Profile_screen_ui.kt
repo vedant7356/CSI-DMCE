e0d7d1df5c7a7b5db7696cb3a66f8b40d72cc57e
Ved <vp7356@gmail.com>
2023-02-17 08:45:25 +0530

    Add profile screen

---
+++ application/app/src/main/java/com/example/csi_dmce/auth/Profile_screen_ui.kt
@@ -0,0 +1,12 @@
+package com.example.csi_dmce.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.csi_dmce.R
+
+class Profile_screen_ui : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_screen_ui)
+    }
+}
